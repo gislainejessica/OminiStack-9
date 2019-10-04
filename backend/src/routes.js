c88746e5b1a9f6a53792401b97fdfa01e056516d
GislaineJessica <GislainehJessica@gmail.com>
2019-10-04 19:53:12 -0300

    Tirando arquivos de lock

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,6 +11,7 @@ const ReservaController = require('./controllers/reservaController')
 const routes = express.Router()
 const upload =  multer(uploadConfig)
 
+// Faz o login do usuario no sistema
 routes.post('/sessions', SessionController.store)
 // Administrador faz upload das imagens
 routes.post('/spots', upload.single('imagem'), SpotController.store)
